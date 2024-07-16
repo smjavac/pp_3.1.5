5e58794f4da7752e7a55330225f43bd1ba404c54
smjavac <sm.yusupov@mail.ru>
2024-07-16 12:55:58 +0300

    logger has been added

--- src/main/java/org/example/config/DBInitialization.java
+++ src/main/java/org/example/config/DBInitialization.java
@@ -3,6 +3,8 @@ package org.example.config;
 import org.example.model.Role;
 import org.example.model.User;
 import org.example.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -12,6 +14,7 @@ import java.util.List;
 @Component
 public class DBInitialization {
     private final UserService userService;
+    private final Logger logger = LoggerFactory.getLogger(DBInitialization.class);
 
     public DBInitialization(UserService userService) {
         this.userService = userService;
@@ -19,14 +22,17 @@ public class DBInitialization {
 
     @PostConstruct
     public void initializeUsers() {
-        User user = userService.findByEmail("admin@mail.ru");
+        User user = userService.findByEmail("admin@gmail.com");
+        if (user != null) { logger.info("user is not null"); }
+
         if (user == null) {
+            logger.info("user is null");
             Role userRole = new Role("ROLE_USER");
             Role adminRole = new Role("ROLE_ADMIN");
             List<Role> adminRoles = new ArrayList<>();
             adminRoles.add(userRole);
             adminRoles.add(adminRole);
-            User admin = new User(adminRoles, "user", "lastname", 30L, "admin@mail.ru", "admin");
+            User admin = new User(adminRoles, "user", "lastname", 30L, "admin@gmail.com", "admin");
 
             userService.save(admin);
 
